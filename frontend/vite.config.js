c8f1ba1c7cc6a00e23abfab9579848477c07c4a2
samgyep <142758464+samgyep@users.noreply.github.com>
2023-11-07 19:21:07 +1300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server:{
     proxy:{
-      '/frontend':'http://localhost:5555'
+      '/backend':'http://localhost:5555'
     }
   },
   plugins: [react()],
