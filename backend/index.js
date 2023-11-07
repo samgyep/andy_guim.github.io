aa46ea97738cec08a0144e3d718cc9dbeb3ab69a
samgyep <142758464+samgyep@users.noreply.github.com>
2023-11-07 23:18:39 +1300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,11 @@ import path from "path";
 dotenv.config();
 const __dirname = path.resolve();
 const app = express();
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
- });
+//app.get("*", (req, res) => {
+ // res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+ //});
 app.use(express.json());
 
 //app.use(cors({origin: "http://localhost:5555"}));
