683b5b08fbd97b8b56b1c5916a239641d1a7488b
samgyep <142758464+samgyep@users.noreply.github.com>
2023-11-07 16:21:22 +1300

    Update DeleteSession.jsx

--- frontend/src/pages/DeleteSession.jsx
+++ frontend/src/pages/DeleteSession.jsx
@@ -13,7 +13,7 @@ const DeleteSession = () => {
   const handleDeleteSession = () => {
     setLoading(true);
     axios
-      .delete(`http://localhost:5555/session/${id}`)
+      .delete(`/backend/session/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Deleted Successfully", { variant: "success" });
