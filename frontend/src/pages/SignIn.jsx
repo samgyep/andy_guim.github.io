333ba11b3d34ca75b0c825f1cabea1929af65348
samgyep <142758464+samgyep@users.noreply.github.com>
2023-11-07 19:20:34 +1300

    Update SignIn.jsx

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -10,7 +10,7 @@ const SignIn = () => {
 
   const handleSubmit = () => {
     axios
-      .get(`http://localhost:5555/user/${email}`)
+      .get(`/backend/user/${email}`)
       .then((res) => {
         const authPassword = res.data.password;
         if (authPassword == passwordEntered) {
